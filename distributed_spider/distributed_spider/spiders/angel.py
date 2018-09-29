40a6fd3c7d6bdd93d224e62301be4d8c6932d613
brady.wang <brady.wang@qq.com>
2018-09-29 11:46:46 +0800

    distributed spider

---
+++ distributed_spider/distributed_spider/spiders/angel.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import time
+
+from scrapy_redis.spiders import RedisSpider
+
+class AngelSpider(RedisSpider):
+    name = "angel"
+    redis_key = "angel:urls"
+    # allowed_domains = ["angelimg.com"]
+    # start_urls = ['http://angelimg.com/']
+    custom_settings = {
+        'REDIS_URL' : 'http://192.168.33.10:6379',
+        'SCHEDULER' : 'scrapy_redis.scheduler.Scheduler',
+        'DUPEFILTER_CLASS' : 'scrapy_redis.dupefilter.RFPDupeFilter'
+    }
+
+    def parse(self, response):
+        time.sleep(1)
+        print(response.url,"++++++++++++++++++++++++++++")
